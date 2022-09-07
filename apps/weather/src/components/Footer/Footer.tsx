964d257cb3e5c5c6bf420a0ecb1ef71a2700f0d9
Roman Velnar <roman.velnar@gmail.com>
2022-09-07 17:25:57 +0100

    added styling to the Header and Footer

--- apps/weather/src/components/Footer/Footer.tsx
+++ apps/weather/src/components/Footer/Footer.tsx
@@ -1,22 +1,27 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 const FooterContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #a5f1e9;
+  padding: 1rem 4rem;
+`;
 
 const FooterTitle = styled.h2`
-    font-size: 3rem;
-`
+  font-size: 1rem;
+`;
 
-const FooterLink = styled.a`
-`
+const FooterLink = styled.a``;
 
 export default function Footer() {
-    return(
-        <FooterContainer>
-            <FooterTitle>Weather App</FooterTitle>
-            <FooterLink href="https://github.com/RomanVelnar/weather-app">Github</FooterLink>
-        </FooterContainer>
-    )
+  return (
+    <FooterContainer>
+      <FooterTitle>Weather App</FooterTitle>
+      <FooterLink href="https://github.com/RomanVelnar/weather-app">
+        Github
+      </FooterLink>
+    </FooterContainer>
+  );
 }
--- apps/weather/src/components/Header/Header.tsx
+++ apps/weather/src/components/Header/Header.tsx
@@ -4,17 +4,21 @@ const HeaderContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: space-around;
+    margin-bottom: 1rem;
 `
 const HeaderTitle = styled.h1`
-    font-size: 4rem;
+    font-size: 1.5rem;
 `
 const ThemeSwitch = styled.button`
     background-color: #555555;
     border: none;
     color: white;
     padding: 0.3rem 0.6rem;
+    border: 2px solid black;
+    border-radius: 10px;
     font-size: 1rem;
+
 `
 
 export default function Header() {
